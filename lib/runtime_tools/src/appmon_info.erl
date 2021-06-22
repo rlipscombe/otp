f0e6f38b5f02061931eeee2f64ecdbb0ab3c974c
Roger Lipscombe <roger@differentpla.net>
2021-06-22 08:09:10 +0100

    Display value of '$process_label' in observer

--- lib/runtime_tools/src/appmon_info.erl
+++ lib/runtime_tools/src/appmon_info.erl
@@ -710,7 +710,15 @@ format(P) when is_pid(P), node(P) /= node() ->
 format(P) when is_pid(P) ->
     case process_info(P, registered_name) of
 	{registered_name, Name} -> atom_to_list(Name);
-	_ -> pid_to_list(P)
+	_ ->
+		case process_info(P, dictionary) of
+			{dictionary, D} ->
+				case proplists:get_value('$process_label', D) of
+					undefined -> pid_to_list(P);
+					Label -> io_lib:format("~tp ~tp", [Label, P])
+				end;
+			_ -> pid_to_list(P)
+		end
     end;
 format(P) when is_port(P) ->
     case erlang:port_info(P, id) of
